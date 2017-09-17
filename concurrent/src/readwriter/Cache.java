47be31c067224288031be33335def9f6a087a782
walmt <304801299@qq.com>
2017-09-17 10:21:35 +0800

    1

---
+++ concurrent/src/readwriter/Cache.java
@@ -0,0 +1,25 @@
+package readwriter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Created by walmt on 2017/9/10.
+ */
+public class Cache {
+    static Map<String, Object> map = new HashMap<>();
+    static ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    static Lock r = rwl.readLock();
+    static Lock w = rwl.writeLock();
+    // 获取一个key对应的value
+    public static final Object get(String key) {
+        r.lock();
+        try {
+            return map.get(key);
+        } finally {
+            r.unlock();
+        }
+    }
+}
