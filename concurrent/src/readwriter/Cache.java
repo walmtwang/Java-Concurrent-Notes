5d7c113770c0f111f2c18f581dcc099981c381ea
walmt <304801299@qq.com>
2017-09-17 22:19:34 +0800

    1

--- concurrent/src/readwriter/Cache.java
+++ concurrent/src/readwriter/Cache.java
@@ -22,4 +22,23 @@ public class Cache {
             r.unlock();
         }
     }
+    // 设置key对应的value，并返回旧的value
+    public static final Object put(String key, Object value) {
+        w.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            w.unlock();
+        }
+    }
+
+    // 清空所有的内容
+    public static final void clear() {
+        w.lock();
+        try {
+            map.clear();
+        } finally {
+            w.unlock();
+        }
+    }
 }
