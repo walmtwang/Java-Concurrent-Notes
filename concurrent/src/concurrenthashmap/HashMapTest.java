004541d6ac1cec88fed022b7dac15d26068b17e4
walmt <304801299@qq.com>
2017-09-21 13:25:50 +0800

    1

--- concurrent/src/concurrenthashmap/HashMapTest.java
+++ concurrent/src/concurrenthashmap/HashMapTest.java
@@ -7,13 +7,21 @@ import java.util.UUID;
  * Created by walmt on 2017/9/19.
  */
 public class HashMapTest {
-    final HashMap<String, String> map = new HashMap<String, String>(2);
-    //    new Thread(() -> {
-//        for (int i = 0; i < 10000; i++) {
-//            new Thread(() -> map.put(UUID.randomUUID().toString(), ""), "ftf" + i).start();
-//        }
-//    }, "ftf");
-    Thread t = new Thread(() -> {
+    final HashMap<String, String> map = new HashMap<>(2);
 
-    })
+    public void hashMapTest() throws InterruptedException {
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                new Thread(() -> map.put(UUID.randomUUID().toString(), ""), "ftf" + i).start();
+            }
+            System.out.println(map.size());
+        }, "ftf");
+        t.start();
+        t.join();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        HashMapTest hashMapTest = new HashMapTest();
+        hashMapTest.hashMapTest();
+    }
 }
