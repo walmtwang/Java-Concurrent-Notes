09264d564ae4fea5830e0e3d13c89dc802dffc36
walmt <304801299@qq.com>
2017-10-04 14:05:55 +0800

    1

--- concurrent/src/threadpoolExecutor/ThreadPoolTest.java
+++ concurrent/src/threadpoolExecutor/ThreadPoolTest.java
@@ -1,12 +1,18 @@
 package threadpoolExecutor;
 
+import java.util.concurrent.Future;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by walmt on 2017/10/1.
  */
 public class ThreadPoolTest {
     public static void main(String[] args) {
-        ThreadPoolExecutor
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 10, TimeUnit.SECONDS, new SynchronousQueue<>());
+        threadPoolExecutor.execute(() -> {
+            // TODO: 2017/10/2
+        });
     }
 }
